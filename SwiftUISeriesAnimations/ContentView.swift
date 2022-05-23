cb01fc1a939c85e53fd9d7abc0f602b70ded575b
Stefan <stefan.blos@gmail.com>
2022-05-23 22:22:07 +0200

    Initial Commit

---
+++ SwiftUISeriesAnimations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUISeriesAnimations
+//
+//  Created by Stefan Blos on 23.05.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUISeriesAnimations/SwiftUISeriesAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISeriesAnimationsApp.swift
+//  SwiftUISeriesAnimations
+//
+//  Created by Stefan Blos on 23.05.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISeriesAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
